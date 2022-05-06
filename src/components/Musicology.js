af991de8a11bbc6bf690aba9b433b6ea60bc3ac2
Samiya <skmcintosh20@gmail.com>
2022-05-05 21:11:50 -0700

    created main components

--- src/components/Musicology.js
+++ src/components/Musicology.js
@@ -1,5 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Body from "./Body";
+import Footer from "./Footer";
 
 export default function Musicology() {
-	return <div>Musicology</div>;
+	return (
+		<Container>
+			<div className="music__Body">
+				{/* SideBar */}
+				<Sidebar />
+				<div className="body">
+					{/* Navbar */}
+					<Navbar />
+					<div className="body__Contents">
+						{/* Body */}
+						<Body />
+					</div>
+				</div>
+			</div>
+			<div className="music_footer">
+				<Footer />
+			</div>
+		</Container>
+	);
 }
+
+const Container = styled.div``;
